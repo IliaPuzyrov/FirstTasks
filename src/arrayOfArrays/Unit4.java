0128bf5d60384351c30290bbdc5b3daf7521c793
IliaPuzyrov <vasa71305@gmail.com>
2020-04-10 10:56:30 +0300

    addind new file

---
+++ src/arrayOfArrays/Unit4.java
@@ -0,0 +1,25 @@
+package arrayOfArrays;
+
+import Utils.Util;
+
+public class Unit4 {
+    public static void main(String[] args) {
+        int a = Util.scan("Vvedite kolichestvo strok");
+        int b =Util.scan("Vvedite kolichestvo stolbcov");
+        int[][] array = Util.arraysOfArrays(a,b);
+        Util.printDouble(array);
+        System.out.println("сумма равна "+findDobleSum(array));
+    }
+    private static int findDobleSum(int[][]x){
+        int sum=0;
+        for(int i=0;i<x.length;i++){
+            for(int j=0;j<x[i].length;j++){
+                sum+=x[i][j];
+
+            }
+        }
+        return sum;
+
+    }
+
+}
