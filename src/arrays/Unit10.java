cb3279fd400ab94b73f90198ba3c2a080b0094c2
IliaPuzyrov <vasa71305@gmail.com>
2020-05-08 19:26:22 +0300

    думал над задачей

--- src/arrays/Unit10.java
+++ src/arrays/Unit10.java
@@ -7,48 +7,44 @@ public class Unit10 {
         int a = Util.scanInt("введите размер массива");
         int[] array = Util.array(a);
         Util.print(array);
-        System.out.println(findMin(array));
+        System.out.println(deleteMin(array));
     }
 
-    public static int findMin(int[] array) {
-        int count = 0;
-        int min = array[0];
-        for (int i = 0; i < array.length; i++) {
-            if (array[i] < min) {
-                array[min] = array[i];
-                count++;
-           //     int[min]= 0;
+    private static int[] deleteMin(int[] array) {
 
-
+            if (array.length == 0) {
+                throw new IllegalArgumentException("Массив нулевой длины!");
             }
+
+            int minCount = 0;
+            int min = array[0];
+
+            for (int i = 1; i < array.length; i++) {
+                if (array[i] <= min) {
+                    min = array[i];
+
+                }
+            }
+
+            for (int value : array) {
+                if (value == min) {
+                    minCount++;
+                }
+            }
+
+            int[] result = new int[array.length - minCount];
+
+            int indexResult = 0;
+            for (int i = 0; i < array.length; i++) {
+                if (array[i] != min) {
+                    result[indexResult] = array[i];
+                    indexResult++;
+                }
+            }
+            return result;
         }
-        System.out.println(min);
-        System.out.println("первенство минимального значения менялось" + count + "раз");
-        return min;
 
     }
 
-    private static void deleteMin(int[] array, int min) {
-        int a = array.length;
-        for (int i = 0; i < array.length - 1; i++) {//сдвиг последующих элементов
-            array[i] = array[i + 1];
-            a--;                            //уменьшение размера
 
-        }
-        for (int j = 0; j < a; j++)         //вывод элементов
-            System.out.print(array[j] + " ");
-        System.out.println("");
-
-
-    }
-
-    private static void a(int[] array, int min) {
-        int[] result = new int[array.length - 1];
-        int index = min;
-        System.arraycopy(array, 0, result, 0, index);
-       System.arraycopy(array, index + 1, result, index, array.length - index - 1);
-
-    }
-
-}
 
